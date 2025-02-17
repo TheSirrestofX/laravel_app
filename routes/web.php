4e4c37df49c99880b1e15adeb7f17a2fcd8b089d
TheSirrestofX <zorenokko04@gmail.com>
2025-02-17 20:26:29 +0800

    added neccessary files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NoteController;
 
+// Define the resource route for notes
+Route::resource('notes', NoteController::class);
+
+// Define the root route
 Route::get('/', function () {
     return view('welcome');
 });
